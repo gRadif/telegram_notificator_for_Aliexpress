f8a873fababe3b4c78294954b51cd11b0f7faffe
gRadif <g.radif88@gmail.com>
2022-02-04 01:41:03 +0300

    change database

--- config.py
+++ config.py
@@ -1,16 +1,7 @@
 import os
 
-DEBUG = True
-access_authentication = False
 
-DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASS')
-DB_HOST = '127.0.0.1'
-DB_PORT = 5432
-
-token = os.getenv('TOKEN')
-
+access_authentication = True
 
 if DEBUG is True:
     try:
